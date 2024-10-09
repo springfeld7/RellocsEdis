32996c2ff964262d9fdaf6e17fa68b89667c5395
thsp1900 <thsp1900@student.miun.se>
2024-10-09 10:09:50 +0200

    Added simple test level

---
+++ app/src/main/java/com/thomasspringfeldt/rellorcsedis/TestLevel.kt
@@ -0,0 +1,21 @@
+package com.thomasspringfeldt.rellorcsedis
+
+/**
+ * A test level.
+ * @author Thomas Springfeldt
+ */
+class TestLevel : LevelData() {
+    init {
+        tileToBitMap[NO_TILE] = "no_tile"
+        tileToBitMap[1] = PLAYER
+        tileToBitMap[2] = "ground_left"
+        tileToBitMap[3] = "ground"
+        tileToBitMap[4] = "ground_right"
+
+        tiles = arrayOf(
+            intArrayOf(2,3,4,0,1)
+        )
+
+        updateLevelDimension()
+    }
+}
