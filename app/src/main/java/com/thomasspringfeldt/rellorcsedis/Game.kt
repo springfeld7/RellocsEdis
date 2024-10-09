28ccb2d8e39520c5c98973426c4ccdc042bde895
thsp1900 <thsp1900@student.miun.se>
2024-10-09 10:58:28 +0200

    Updated update function

--- app/src/main/java/com/thomasspringfeldt/rellorcsedis/Game.kt
+++ app/src/main/java/com/thomasspringfeldt/rellorcsedis/Game.kt
@@ -1,10 +1,8 @@
 package com.thomasspringfeldt.rellorcsedis
 
-import android.annotation.SuppressLint
 import android.content.Context
-import android.graphics.Canvas
 import android.graphics.Color
-import android.graphics.Paint
+
 import android.os.Build
 import android.os.SystemClock.uptimeMillis
 import android.util.Log
@@ -19,7 +17,7 @@ const val TARGET_FPS = 60f
 var RNG = Random(uptimeMillis())
 
 /**
- * Game engine for the Space Shooter.
+ * Game engine for Rellorcs Edis.
  * @author Thomas Springfeldt
  */
 class Game(context: Context) : SurfaceView(context), Runnable, SurfaceHolder.Callback {
@@ -27,6 +25,7 @@ class Game(context: Context) : SurfaceView(context), Runnable, SurfaceHolder.Cal
     private val tag = "Game"
     private lateinit var gameThread : Thread
     @Volatile private var isRunning : Boolean = false
+    private val level: LevelManager = LevelManager(TestLevel())
 
     init {
         holder?.addCallback(this)
@@ -37,7 +36,7 @@ class Game(context: Context) : SurfaceView(context), Runnable, SurfaceHolder.Cal
      * Game loop.
      */
     override fun run() {
-        Log.d(tag, "Run()")
+        Log.d(tag, "run()")
         while(isRunning) {
             update()
             render()
@@ -48,7 +47,7 @@ class Game(context: Context) : SurfaceView(context), Runnable, SurfaceHolder.Cal
      * Updates game state.
      */
     private fun update() {
-
+        level.update(0.1f) //fix later
     }
 
     /**
@@ -56,7 +55,13 @@ class Game(context: Context) : SurfaceView(context), Runnable, SurfaceHolder.Cal
      */
     private fun render() {
         val canvas = holder?.lockCanvas() ?: return
-        canvas.drawColor(Color.CYAN)
+        val random = RNG.nextInt(2)
+        if (random == 0) {
+            canvas.drawColor(Color.MAGENTA)
+        } else {
+            canvas.drawColor(Color.CYAN)
+        }
+
         holder.unlockCanvasAndPost(canvas)
     }
 
@@ -92,7 +97,7 @@ class Game(context: Context) : SurfaceView(context), Runnable, SurfaceHolder.Cal
     }
 
     override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
-        Log.d(tag, "SurfaceChanged(width: $width, height: $height")
+        Log.d(tag, "SurfaceChanged(width: $width, height: $height)")
     }
 
     override fun surfaceDestroyed(holder: SurfaceHolder) {
