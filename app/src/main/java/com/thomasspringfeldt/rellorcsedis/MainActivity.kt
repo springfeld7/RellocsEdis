9bb0386c6d658076c3c1479e693323232c5a5b74
thsp1900 <thsp1900@student.miun.se>
2024-10-09 09:10:56 +0200

    Enabled fullscreen

--- app/src/main/java/com/thomasspringfeldt/rellorcsedis/MainActivity.kt
+++ app/src/main/java/com/thomasspringfeldt/rellorcsedis/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.thomasspringfeldt.rellorcsedis
 
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
@@ -12,11 +17,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var game : Game
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d(tag, "OnCreate()")
         super.onCreate(savedInstanceState)
+        Log.d(tag, "MainActivity launched")
         game = Game(this)
         setContentView(game)
-
     }
 
     override fun onPause() {
@@ -32,5 +36,39 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         game.onDestroy()
         super.onDestroy()
+    }
+
+    /* CODE FOR ENABLING FULLSCREEN */
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) hideSystemUI()
+    }
+
+    private fun hideSystemUI() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R) { //handle older SDKs, using the deprecated systemUiVisbility API
+            @Suppress("DEPRECATION")
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    // Set the content to appear under the system bars so that the
+                    // content doesn't resize when the system bars hide and show.
+                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    // Hide the nav bar and status bar
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN)
+        } else {
+            // Tell the Window that our app is going to responsible for fitting for any system windows.
+            // This is similar to: view.setSystemUiVisibility(LAYOUT_STABLE | LAYOUT_FULLSCREEN)
+            WindowCompat.setDecorFitsSystemWindows(window, false)
+            // the decor view is the true root of the Window's view hierarchy. It contains both the "decor" (i.e. the window's title (action bar) and also contains the app-supplied content view.
+            //we need the root view so we can get its insetController
+            val insetsController = window.decorView.windowInsetsController ?: return
+            //Hide the keyboard (IME = "input method editor")
+            insetsController.hide(WindowInsets.Type.ime())
+            // set the modern equivalent to Sticky Immersive Mode:
+            insetsController.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            // hide both the system bars (eg. the status and navigation bars):
+            insetsController.hide(WindowInsets.Type.systemBars())
+        }
     }
 }
