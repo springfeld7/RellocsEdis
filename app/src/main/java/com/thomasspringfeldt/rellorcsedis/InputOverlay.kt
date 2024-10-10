903170698ea66f0eb23735d249a0b4e982395d28
thsp1900 <thsp1900@student.miun.se>
2024-10-10 11:26:23 +0200

    Added virtual input controller

---
+++ app/src/main/java/com/thomasspringfeldt/rellorcsedis/InputOverlay.kt
@@ -0,0 +1,11 @@
+package com.thomasspringfeldt.rellorcsedis
+
+open class InputOverlay {
+    var verticalFactor = 0.0f
+    var horizontalFactor = 0.0f
+    var isJumping = false
+    fun onStart() {}
+    fun onStop() {}
+    fun onPause() {}
+    fun onResume() {}
+}
