f38a93054494160f5c7d537890b0f2c2bb1980a3
thsp1900 <thsp1900@student.miun.se>
2024-10-15 10:31:42 +0200

    Added a composite control manager to enable multiple controller inputs

---
+++ app/src/main/java/com/thomasspringfeldt/rellorcsedis/CompositeControl.kt
@@ -0,0 +1,59 @@
+package com.thomasspringfeldt.rellorcsedis
+
+class CompositeControl(vararg inputs: InputManager) : InputManager() {
+    private val inputs = ArrayList<InputManager>()
+
+    init {
+        for (im in inputs) {
+            this.inputs.add(im)
+        }
+    }
+
+    fun addInput(im: InputManager) {
+        inputs.add(im)
+    }
+
+    fun setInput(im: InputManager) {
+        onPause()
+        onStop()
+        inputs.clear()
+        addInput(im)
+    }
+
+    override fun update(dt: Float) {
+        isJumping = false
+        horizontalFactor = 0f
+        verticalFactor = 0f
+        for (im in inputs) {
+            im.update(dt)
+            isJumping = isJumping || im.isJumping
+            horizontalFactor += im.horizontalFactor
+            verticalFactor += im.verticalFactor
+        }
+        clampInputs()
+    }
+
+    override fun onStart() {
+        for (im in inputs) {
+            im.onStart()
+        }
+    }
+
+    override fun onStop() {
+        for (im in inputs) {
+            im.onStop()
+        }
+    }
+
+    override fun onPause() {
+        for (im in inputs) {
+            im.onPause()
+        }
+    }
+
+    override fun onResume() {
+        for (im in inputs) {
+            im.onResume()
+        }
+    }
+}
